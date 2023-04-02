803dc1e7b740b2ad75ee0aba48a0ca29aa18da83
Jacob Fahnestock <jrfz31@gmail.com>
2023-04-01 22:50:22 -0400

    feat: first pass at responsive map page

--- src/pages/ChartPage.tsx
+++ src/pages/ChartPage.tsx
@@ -10,6 +10,8 @@ import { findManyTimeseries } from "../utils/findManyTimeseries";
 import { useSearchParams } from "react-router-dom";
 import { urlParamsToMarkers } from "../utils/markerParamUtilities";
 import { ShareButton } from "../components/ShareButton";
+//@ts-ignore
+import { useBreakpoints } from 'react-breakpoints-hook'
 
 export type ITimeseries = {
   marker: IMarker
@@ -21,6 +23,13 @@ type IProps = {
 }
 
 const ChartPage = (props: IProps) => {
+  let { sm, md, lg } = useBreakpoints({
+    sm: { min: 0, max: 860 },
+    md: { min: 861, max: 1400 },
+    lg: { min: 1401, max: null },
+  });
+  
+
   const [timeseriesArr, setTimeseriesArr] = React.useState<Array<ITimeseries>>([])
   const [progress, setProgress] = React.useState<number>(0)
 
@@ -39,50 +48,97 @@ const ChartPage = (props: IProps) => {
   }, [markers])
 
   return (
-    <div className="w-full h-[89vh]">
-      <ProgressBarWithTimer numOfMarkers={markers.length} disabled={!(timeseriesArr.length === 0)} setProgress={setProgress} progress={progress} />
-      <BackButton params={params}/>
-      <div className="w-full h-full grid grid-cols-3 grid-rows-1 gap-4">
+    <>
+      {lg &&
+        <div className="w-full h-[89vh]">
+          <ProgressBarWithTimer numOfMarkers={markers.length} disabled={!(timeseriesArr.length === 0)} setProgress={setProgress} progress={progress} />
+          <BackButton params={params} />
+          <div className="w-full h-full grid grid-cols-3 grid-rows-1 gap-4">
 
-        <div className="col-span-2">
-          <ZoomingChart timeseriesArr={timeseriesArr} />
-        </div>
+            <div className="col-span-2">
+              <ZoomingChart timeseriesArr={timeseriesArr} />
+            </div>
 
-        <div className="mr-5">
-          <div className="w-[100%] h-[40%] ">
-            <Velmap
-              zoom={7}
-              center={markers[0] ? [markers[0].latLng.lat, markers[0].latLng.lng] : [70, -50]}
-              mapChildren={
-                <>
-                  {
-                    markers.map(marker => (
-                      <LocationMarker key={`${marker.id}`} markerProp={marker} markers={markers} draggable={false} />
-                    ))
+            <div className="mr-5">
+              <div className="w-[100%] h-[40%] ">
+                <Velmap
+                  zoom={7}
+                  center={markers[0] ? [markers[0].latLng.lat, markers[0].latLng.lng] : [70, -50]}
+                  mapChildren={
+                    <>
+                      {
+                        markers.map(marker => (
+                          <LocationMarker key={`${marker.id}`} markerProp={marker} markers={markers} draggable={false} />
+                        ))
+                      }
+                    </>
                   }
-                </>
-              }
-            />
-          </div>
-          <div className="mt-4">
-            <MarkerTable markers={markers} />
-          </div>
-          <div className="mt-4">
-            <CSVDownloadButton data={timeseriesArr} />
+                />
+              </div>
+              <div className="mt-4">
+                <MarkerTable markers={markers} />
+              </div>
+              <div className="mt-4">
+                <CSVDownloadButton data={timeseriesArr} />
+
+              </div>
+              <div className="mt-4">
+                <ShareButton />
+              </div>
+            </div>
 
           </div>
-          <div className="mt-4">
-            <ShareButton />
+          <div className="w-full h-[50px]  flex flex-row justify-center">
+            <div className="flex flex-col justify-center">
+              <div className="">Velocity data generated using auto-RIFT (Gardner et al., 2018) and provided by the NASA MEaSUREs ITS_LIVE project (Gardner et al., 20XX).</div>
+            </div>
           </div>
         </div>
+      }
+      {(md || sm) &&
+        <div className="w-full h-full">
+          <ProgressBarWithTimer numOfMarkers={markers.length} disabled={!(timeseriesArr.length === 0)} setProgress={setProgress} progress={progress} />
+          <BackButton params={params} />
+          <div className="w-full h-full flex flex-col justify-center items-center ">
 
-      </div>
-      <div className="w-full h-[50px]  flex flex-row justify-center">
-        <div className="flex flex-col justify-center">
-          <div className="">Velocity data generated using auto-RIFT (Gardner et al., 2018) and provided by the NASA MEaSUREs ITS_LIVE project (Gardner et al., 20XX).</div>
+            <div className="">
+              <ZoomingChart timeseriesArr={timeseriesArr} />
+            </div>
+
+              <div className="w-[100%] h-[40%] ">
+                <Velmap
+                  zoom={7}
+                  center={markers[0] ? [markers[0].latLng.lat, markers[0].latLng.lng] : [70, -50]}
+                  mapChildren={
+                    <>
+                      {
+                        markers.map(marker => (
+                          <LocationMarker key={`${marker.id}`} markerProp={marker} markers={markers} draggable={false} />
+                        ))
+                      }
+                    </>
+                  }
+                />
+              </div>
+              <div className="mt-4">
+                <MarkerTable markers={markers} />
+              </div>
+              <div className="mt-4">
+                <CSVDownloadButton data={timeseriesArr} />
+
+              </div>
+              <div className="mt-4">
+                <ShareButton />
+              </div>
+          </div>
+          <div className="w-full flex flex-row justify-center">
+            <div className="flex flex-col justify-center">
+              <div className="mx-2 my-2">Velocity data generated using auto-RIFT (Gardner et al., 2018) and provided by the NASA MEaSUREs ITS_LIVE project (Gardner et al., 20XX).</div>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      }
+    </>
   )
 };
 
