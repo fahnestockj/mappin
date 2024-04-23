06b8fb9d0548d9d72a0a10cb5522db759649fa3d
fahnestockj <jrfz31@gmail.com>
2024-04-23 19:32:00 -0400

    fix: csv download

--- src/components/CSVDownloadButton.tsx
+++ src/components/CSVDownloadButton.tsx
@@ -1,14 +1,14 @@
-import { BiDownload } from 'react-icons/bi';
-import { saveAs } from 'file-saver'
-import JSZip from 'jszip';
-import { unparse } from 'papaparse';
-import { ITimeseries } from '../types';
+import { BiDownload } from "react-icons/bi";
+import { saveAs } from "file-saver";
+import JSZip from "jszip";
+import { unparse } from "papaparse";
+import { ITimeseries } from "../types";
 
 type IProps = {
-  data: ITimeseries[]
-}
+  data: ITimeseries[];
+};
 export const CSVDownloadButton = (props: IProps) => {
-  const { data } = props
+  const { data } = props;
   const zip = new JSZip();
 
   return (
@@ -17,18 +17,25 @@ export const CSVDownloadButton = (props: IProps) => {
       className="cursor-pointer inline-flex items-center rounded-md border border-transparent bg-sky-700 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-sky-800 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
       onClick={() => {
         data.forEach((timeseries) => {
-          const csvStr = unparse([['mid_date', 'v [m/yr]'], [timeseries.data.midDateArray, timeseries.data.velocityArray]])
-          zip.file(`lat_${timeseries.marker.latLon.lat}_lon_${timeseries.marker.latLon.lon}.csv`, csvStr)
-        })
+          let csvStr = "mid_date, v [m/yr]\n";
+          timeseries.data.midDateArray.forEach(
+            (midDate, index) =>
+              (csvStr += `${midDate.toDateString()},${timeseries.data.velocityArray[index]}\n`)
+          );
+          
+          zip.file(
+            `lat_${timeseries.marker.latLon.lat}_lon_${timeseries.marker.latLon.lon}.csv`,
+            csvStr
+          );
+        });
 
-        zip.generateAsync({ type: "blob" })
-          .then(function (content) {
-            saveAs(content, 'itslive-data.zip');
-          });
+        zip.generateAsync({ type: "blob" }).then(function (content) {
+          saveAs(content, "itslive-data.zip");
+        });
       }}
     >
-      <BiDownload className='scale-150 mr-3 mb-1' />
+      <BiDownload className="scale-150 mr-3 mb-1" />
       Download CSV
     </button>
-  )
+  );
 };
