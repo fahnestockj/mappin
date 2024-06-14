295676d8ec2645b372f394ce742f60c74fd45b6d
fahnestockj <jrfz31@gmail.com>
2024-06-14 17:21:12 -0400

    chore: routing for the s3 bucket

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/app'> {/* NOTE: basename in dev the basename is /itslive-wev/app */}
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/chart" element={<App />} /> {/* NOTE: supporting old routing */}
